8f5b1d343477490df5192ee8a76773573fbb5c1a
Sebastian Pina Otey <spinaotey@gmail.com>
2024-01-26 20:13:55 +0100

    Particles!

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,36 +9,40 @@ var can_grenade: bool = true
 var _laser_start_positions: Array[Node]
 
 func _ready():
-    _laser_start_positions = $LaserStartPositions.get_children()
+	_laser_start_positions = $LaserStartPositions.get_children()
 
 func _process(_delta):
 
-    # input movement
-    var direction = Input.get_vector("left", "right", "up", "down")
-    velocity = direction * speed
-    move_and_slide()
+	# input movement
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
 
-    # rotate
-    look_at(get_global_mouse_position())
+	# rotate
+	look_at(get_global_mouse_position())
 
-    # laser shoot
-    if Input.is_action_pressed("primary action") and can_laser:
-        var laser_postion = _laser_start_positions.pick_random().global_position
-        var laser_direction = global_position.direction_to(get_global_mouse_position()).normalized()
-        laser.emit(laser_postion,laser_direction)
-        can_laser = false
-        $LaserTimer.start()
+	# laser shoot
+	if Input.is_action_pressed("primary action") and can_laser:
+		var laser_postion = _laser_start_positions.pick_random().global_position
+		var laser_direction = global_position.direction_to(get_global_mouse_position()).normalized()
+		var particles: GPUParticles2D = $ShootParticles
+		if particles.emitting:
+			particles.restart()
+		particles.emitting = true
+		laser.emit(laser_postion,laser_direction)
+		can_laser = false
+		$LaserTimer.start()
 
-    # shoot grenade
-    if Input.is_action_pressed("secondary action") and can_grenade:
-        var grenade_postion = _laser_start_positions.pick_random().global_position
-        var grenade_direction = global_position.direction_to(get_global_mouse_position()).normalized()
-        grenade.emit(grenade_postion, grenade_direction)
-        can_grenade = false
-        $GrenadeTimer.start()
+	# shoot grenade
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		var grenade_postion = _laser_start_positions.pick_random().global_position
+		var grenade_direction = global_position.direction_to(get_global_mouse_position()).normalized()
+		grenade.emit(grenade_postion, grenade_direction)
+		can_grenade = false
+		$GrenadeTimer.start()
 
 func _on_laser_timer_timeout():
-    can_laser = true
+	can_laser = true
 
 func _on_grenade_timer_timeout():
-    can_grenade = true
+	can_grenade = true
