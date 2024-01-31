3a88ffdb14e5457be114b91c3c0db2f273be3302
Sebastian Pina Otey <spinaotey@gmail.com>
2024-01-31 06:56:05 +0100

    Fixed inside and outside

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -2,5 +2,4 @@ extends Level
 
 
 func _on_exit_gate_body_entered(_body:Node2D):
-    var tween = create_tween()
-    tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,9 +1,7 @@
 extends Level
 
 func _on_gate_player_entered_gate(_body:Node2D):
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.5)
-
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/levels/inside.tscn")
 
 func _on_house_player_entered():
 	var tween = create_tween()
