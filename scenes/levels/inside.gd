db73f387522a240b4a6986d3e938fe9f1e9dea7e
Sebastian Pina Otey <spinaotey@gmail.com>
2024-02-02 06:35:32 +0100

    UI setup

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -2,4 +2,6 @@ extends Level
 
 
 func _on_exit_gate_body_entered(_body:Node2D):
+	var tween = create_tween()
+	tween.tween_property($Player,"speed",0, .5)
 	get_tree().call_deferred("change_scene_to_file", "res://scenes/levels/outside.tscn")
